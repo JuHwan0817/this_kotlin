41e0b6b368807f4438cfbd81f7f5e3d49de1d00f
juhwan203 <juhwan203@naver.com>
2022-10-12 16:37:44 +0900

    1012

--- Inheritanceandextension/app/src/main/java/com/juhwan203/inheritanceandextension/MainActivity.kt
+++ Inheritanceandextension/app/src/main/java/com/juhwan203/inheritanceandextension/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.juhwan203.inheritanceandextension
+package com.ashram7.inheritanceandextension
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,82 +9,60 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //  1. 부모 클래스 직접 호출하기
+        // 1. 부모 클래스 직접 호출하기
         var parent = Parent()
         parent.sayHello()
-        //  1. 자식 클래스 호출해서 사용하기
-        var child = child()
+        // 1. 자식 클래스 호출해서 사용하기
+        var child = Child()
         child.myHello()
 
         testStringExtension()
     }
-    //String 익스텐션 테스트 하기
-    /*fun testStringExtension(){
+    // String 익스텐션 테스트 하기
+    fun testStringExtension() {
         var original = "Hello"
         var added = " Guys~"
-        //  plus 함수를 사용해서 문자열을 더할 수 있다.
-        Log.d("Extension", "added를 더한값은 ${original.plus(added)}입니다")
-    }*/
+        // plus 함수를 사용해서 문자열을 더할 수 있다.
+        Log.d("Extension"," added를 더한값은 ${original.plus(added)}입니다")
+    }
 }
 // 상속 연습
 open class Parent {
     var hello:String = "안녕하세요"
-    fun sayHello(){
-        Log.d("inheritance","${hello}")
+    fun sayHello( ) {
+        Log.d("inheritance", "${hello}")
     }
 }
-open class Parent() {
-    fun myHello(){
-        hello = "hello"
-        sayHello()
+class Child : Parent( ) {
+    fun myHello( ) {
+        hello = "Hello"
+        sayHello( )
     }
 }
-open class BaseClass{
-    open fun opend(){
+// 메서드 오버라이드 연습
+open class BaseClass {
+    open fun opened() {
 
     }
-    fun notOpend(){
+    fun notOpend() {
 
     }
 }
-class ChildClass: BaseClass{
-    override fun opend() {
+class ChildClass: BaseClass( ) {
+    override fun opened() {
 
     }
-    // override fun notOpend(){
+//    override fun notOpend(){ // 오버라이드 되지 않고 에러가 발생한다.
 //
-// }
+//    }
 }
 // 프로퍼티 오버라이드 연습
-open class BaseClass2{
+open class BaseClass2 {
     open var opened: String = "I am"
 }
-class ChildClass2: BaseClass2(){
+class ChildClass2: BaseClass2( ) {
     override var opened: String = "You are"
 }
-fun String.plus(word:String):String{
+fun String.plus(word:String ) : String{
     return this + word
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
