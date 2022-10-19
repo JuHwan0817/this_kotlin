bfe8c7c32cc922067c70addf4563f3d07d49811f
juhwan203 <juhwan203@naver.com>
2022-10-19 12:03:54 +0900

    Recyclerbinding

--- ContainerRecyclerView/app/src/main/java/com/juhwan203/containerrecyclerview/MainActivity.kt
+++ ContainerRecyclerView/app/src/main/java/com/juhwan203/containerrecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.juhwan203.containerrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.juhwan203.containerrecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val data: MutableList<Memo> = loadData()
+        var adapter = CustomAdapter()
+        adapter.listData = data
+        binding.recyclerView.adapter = adapter
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
     //100개의 list를 가상으로 만듭니다.
