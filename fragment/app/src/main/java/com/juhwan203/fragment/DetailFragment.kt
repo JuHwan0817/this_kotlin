0d869e3eaac3eab9ecfe92478ea3504d91002a49
juhwan203 <juhwan203@naver.com>
2022-10-20 12:53:53 +0900

    fragment

--- fragment/app/src/main/java/com/juhwan203/fragment/DetailFragment.kt
+++ fragment/app/src/main/java/com/juhwan203/fragment/DetailFragment.kt
@@ -6,31 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.juhwan203.fragment.databinding.FragmentListBinding
+import com.juhwan203.fragment.databinding.FragmentDetailBinding
 
-class ListFragment : Fragment() {
+class DetailFragment : Fragment() {
     var mainActivity:MainActivity? = null
-
-    lateinit var binding:FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentListBinding.inflate(inflater, container, false)
-
-        binding.btnNext.setOnClickListener { mainActivity?.goDetail() }
-
-        binding.textTitle.text = arguments?.getString("key1")
-        binding.textValue.text = "${arguments?.getInt("key2")}"
-
+        val binding = FragmentDetailBinding.inflate(inflater,container,false)
+        binding.btnBack.setOnClickListener { mainActivity?.goBack() }
         return binding.root
     }
-
-    fun setValue(value:String) {
-        binding.textFromActivity.text = value
-    }
-
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
--- fragment/app/src/main/java/com/juhwan203/fragment/MainActivity.kt
+++ fragment/app/src/main/java/com/juhwan203/fragment/MainActivity.kt
@@ -2,23 +2,54 @@ package com.juhwan203.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.juhwan203.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         setFragment()
+
+        binding.btnSend.setOnClickListener {
+            listFragment.setValue("전달할 값")
+        }
     }
 
-    fun setFragment(){
-        //ListFragment 를 생성
-        val listFragment: ListFragment = ListFragment()
-        // 프래그먼트 매니저를 통해서 트랜잭션을 시작하고, 시작한 트랜잭션을 변수에 저장
+    lateinit var listFragment:ListFragment
+
+    fun setFragment() {
+        listFragment = ListFragment()
+
+        var bundle = Bundle()
+        bundle.putString("key1", "List Fragment")
+        bundle.putInt("key2", 20170727)
+        listFragment.arguments = bundle
+
         val transaction = supportFragmentManager.beginTransaction()
-        // 트랜잭션의 add() 메서드로 frmeLayout을 id로 가지고 있는 레이아웃 앞에서 생성한 listFragment 삽입
         transaction.add(R.id.frameLayout, listFragment)
-        // setFragment() 메서드 안에 다음과 같이 ListFragment를 생성
         transaction.commit()
     }
+
+    fun goDetail(){
+        val detailFragment = DetailFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, detailFragment)
+        transaction.addToBackStack("detail")
+        transaction.commit()
+    }
+
+    fun goBack(){
+        onBackPressed()
+    }
 }
+
+
+
+
+
+
