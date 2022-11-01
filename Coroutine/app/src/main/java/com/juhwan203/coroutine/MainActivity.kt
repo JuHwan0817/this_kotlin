6235065e7c0e4a5077f2adfe92610400f38c9003
juhwan203 <juhwan203@naver.com>
2022-11-01 11:36:47 +0900

    Coroutine

--- Coroutine/app/src/main/java/com/juhwan203/coroutine/MainActivity.kt
+++ Coroutine/app/src/main/java/com/juhwan203/coroutine/MainActivity.kt
@@ -9,24 +9,33 @@ import com.juhwan203.coroutine.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.buttonDownload.setOnClickListener{
-            CoroutineScope(Dispatchers.Main).launch{
-                binding.progress.visibility = View.VISIBLE
-                val url = binding.editUrl.text.toString()
+        binding.run{
+            buttonDownload.setOnClickListener {
+                CoroutineScope(Dispatchers.Main).launch {
+                    progress.visibility = View.VISIBLE
+                    val url = editUrl.text.toString()
+                    val bitmap = withContext(Dispatchers.IO) {
+                        loadImage(url)
+                    }
+                    imagePreview.setImageBitmap(bitmap)
+                    progress.visibility = View.GONE
+                }
             }
         }
     }
 }
 
-suspend fun  loadImage(imageUrl:String) : Bitmap {
+suspend fun loadImage(imageUrl:String) : Bitmap {
     val url = URL(imageUrl)
     val stream = url.openStream()
     return BitmapFactory.decodeStream(stream)
